5320f9c721e89a7b57455a5bcf6a4514732c10d8
Gil Forsyth <gil@forsyth.dev>
2024-05-14 17:27:17 -0400

    chore: fix up load scripts
    no more pypi data in postgres

--- verify.sql
+++ verify.sql
@@ -1,7 +1,3 @@
-SELECT 'deps' table, COUNT(*) n FROM deps UNION
-SELECT 'maintainers' table, COUNT(*) n FROM maintainers UNION
-SELECT 'package_urls' table, COUNT(*) n FROM package_urls UNION
-SELECT 'packages' table, COUNT(*) n FROM packages UNION
-SELECT 'scorecard_checks' table, COUNT(*) n FROM scorecard_checks UNION
-SELECT 'wheels' table, COUNT(*) n FROM wheels
+SELECT 'imdb_title_basics' table, COUNT(*) n FROM imdb_title_basics UNION
+SELECT 'imdb_title_ratings' table, COUNT(*) n FROM imdb_title_ratings UNION
 ORDER BY "table";
