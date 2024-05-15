453039bd79422296b870fa1a28500ed45e77db41
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-05-15 08:09:38 -0400

    chore: render notebooks

--- quarto/render.sh
+++ quarto/render.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 for file in *.qmd; do
-    filename=$(basename -- "$file" .qmd)
-    quarto convert "$file" --output "../$filename.ipynb"
+  filename="$(basename -- "$file" .qmd)"
+  quarto convert "$file" --output "../$filename.ipynb"
 done
-
