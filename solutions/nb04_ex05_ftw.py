04f7993d138a1bbd4d3bdc24e957eb9e3924e09d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-01 08:05:17 -0400

    chore: update to compute rather than show the scalar repr

--- solutions/nb04_ex05_ftw.py
+++ solutions/nb04_ex05_ftw.py
@@ -1,3 +1,5 @@
 ftw_packages = maintainer_counts.filter(_.package_name.startswith("ftw-"))
 
-print(f"{maintainer_counts.count()=}, {ftw_packages.count()=}")
+maint_counts = maintainer_counts.count().to_pandas()
+ftw_counts = ftw_packages.count().to_pandas()
+print(f"{maint_counts=}, {ftw_counts=}")
