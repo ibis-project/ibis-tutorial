d0da9685455d8b52cc779a5702f1dba3a2f9e9eb
Jim Crist-Harif <jcristharif@gmail.com>
2024-07-08 01:24:16 -0500

    A few tiny fixups
    Some formatting fixups found while running through the tutorial on
    codespaces.

--- solutions/nb01_ex04.py
+++ solutions/nb01_ex04.py
@@ -1,6 +1,7 @@
-(
+mean_mass_per_island = (
     penguins
     .group_by("island")
     .agg(island_mean_mass=penguins.body_mass_g.mean())
-    .join(penguins, "island")
 )
+
+penguins.join(mean_mass_per_island, "island")
